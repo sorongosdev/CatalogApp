7c15189868c7a6663b4578c47bd0c39633de6d19
Sorongos <dosora9@gmail.com>
2025-01-20 01:55:38 +0900

    Modifier: 버튼 아이콘 변경

--- app/src/main/java/com/example/catalogapp/MainActivity.kt
+++ app/src/main/java/com/example/catalogapp/MainActivity.kt
@@ -1,19 +1,14 @@
 package com.example.catalogapp
 
 import android.os.Bundle
-import android.view.RoundedCorner
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.Send
+import androidx.compose.material.icons.filled.Search
 
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -21,9 +16,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.catalogapp.ui.theme.CatalogAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,36 +25,35 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CatalogAppTheme {
-                Greeting(onButtonClicked = {
-                    Toast.makeText(this,"Send Clicked", Toast.LENGTH_SHORT).show()
-                })
+                Greeting()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(onButtonClicked: () -> Unit) {
+fun Greeting() {
     Button(
-        onClick = onButtonClicked,
-        enabled = true,
-        border = BorderStroke(10.dp, Color.Magenta),
-        shape = RoundedCornerShape(10.dp),
-        contentPadding = PaddingValues(20.dp)
-    ){
+        onClick = {},
+        modifier = Modifier.fillMaxSize()
+    ) {
         Icon(
-            imageVector = Icons.AutoMirrored.Filled.Send,
+            imageVector = Icons.Filled.Search,
             contentDescription = null
         )
-        Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
-        Text(text = "Send")
+        Spacer(
+            modifier = Modifier.size(ButtonDefaults.IconSpacing)
+        )
+        Text(text = "Search")
     }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+)
 @Composable
 fun GreetingPreview() {
     CatalogAppTheme {
-        Greeting(onButtonClicked={})
+        Greeting()
     }
 }
