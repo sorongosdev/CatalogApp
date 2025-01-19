eeb650f8f3ab89a9f873a7b7750e86c8c77d6f31
Sorongos <dosora9@gmail.com>
2025-01-20 00:24:15 +0900

    Text 컴포넌트

--- app/src/main/java/com/example/catalogapp/MainActivity.kt
+++ app/src/main/java/com/example/catalogapp/MainActivity.kt
@@ -6,11 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.catalogapp.ui.theme.CatalogAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,8 +42,16 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        modifier = Modifier.size(300.dp),
+        color = Color.Red,
+        text = "Hello $name!\nHello $name!\nHello $name!\n",
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = FontFamily.Cursive,
+        letterSpacing = 2.sp,
+        maxLines = 2,
+        textDecoration = TextDecoration.Underline,
+        textAlign = TextAlign.Justify
     )
 }
 
