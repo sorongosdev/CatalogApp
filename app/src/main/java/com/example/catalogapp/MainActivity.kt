e255c76fea523b157875c3ba67374c36993b4875
Sorongos <dosora9@gmail.com>
2025-01-20 02:07:26 +0900

    colors로 버튼 색상

--- app/src/main/java/com/example/catalogapp/MainActivity.kt
+++ app/src/main/java/com/example/catalogapp/MainActivity.kt
@@ -5,9 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 
@@ -17,6 +15,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.catalogapp.ui.theme.CatalogAppTheme
@@ -38,7 +38,12 @@ fun Greeting() {
     Button(
         onClick = {},
         modifier = Modifier
-            .size(200.dp,100.dp)
+            .size(200.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Black,
+            contentColor = Color.Cyan
+        ),
+        shape = RectangleShape
     ) {
         Icon(
             imageVector = Icons.Filled.Search,
