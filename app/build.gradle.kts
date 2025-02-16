3d55f176d803b6ea4df6f8a33b7d42a95c2cb743
Sorongos <61692372+sorongosdev@users.noreply.github.com>
2025-02-16 22:15:28 +0900

    Network Image: okhttp 라이브러리 추가
    이거 없으면 이미지 안 뜸

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,8 +49,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.coil.core)
     implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
