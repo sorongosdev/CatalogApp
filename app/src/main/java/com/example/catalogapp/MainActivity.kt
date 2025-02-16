0b54e703c392c20faf5e61d3c01073d9d1061763
Sorongos <61692372+sorongosdev@users.noreply.github.com>
2025-02-16 19:02:41 +0900

    Image: 이미지 표출

--- app/src/main/java/com/example/catalogapp/MainActivity.kt
+++ app/src/main/java/com/example/catalogapp/MainActivity.kt
@@ -4,17 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.catalogapp.ui.theme.CatalogAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,36 +17,22 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CatalogAppTheme {
-                Outer()
+                Greeting()
             }
         }
     }
 }
 
 @Composable
-fun Outer() {
-    Column(
-        Modifier.height(200.dp)  // Column 자체의 높이를 지정
-    ) {
-        Inner(modifier = Modifier
-            .widthIn(200.dp)
-            .heightIn(160.dp)
+fun Greeting() {
+    Column {
+        Image(
+            painter = painterResource(id = R.drawable.wall),
+            contentDescription = "엔텔로프 캐년"
         )
     }
 }
 
-@Composable
-fun Inner(modifier: Modifier = Modifier) {
-    BoxWithConstraints(modifier) {
-        if (maxHeight > 150.dp) {
-            Text(
-                text = "여기 꽤 길군요...",
-                modifier = Modifier.align(Alignment.BottomCenter)
-            )
-        }
-        Text("maxW: $maxWidth maxH: $maxHeight minW: $minWidth minH: $minHeight")
-    }
-}
 
 
 @Preview(
@@ -61,6 +41,6 @@ fun Inner(modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     CatalogAppTheme {
-        Outer()
+        Greeting()
     }
 }
