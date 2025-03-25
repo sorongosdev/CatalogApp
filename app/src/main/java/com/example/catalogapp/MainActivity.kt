c7d895821d8fe06e2953e4ee17dffbb95b61aadb
Sorongos <dosora9@gmail.com>
2025-03-25 15:50:42 +0900

    CheckBox: by를 이용한 위임

--- app/src/main/java/com/example/catalogapp/MainActivity.kt
+++ app/src/main/java/com/example/catalogapp/MainActivity.kt
@@ -19,8 +19,10 @@ import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -50,11 +52,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun CheckBoxEx() {
     Row(verticalAlignment = Alignment.CenterVertically){
-        var checked = remember {mutableStateOf(false)}
+        var checked by remember {mutableStateOf(false)}
         Checkbox(
-            checked = checked.value,
+            checked = checked,
             onCheckedChange = {
-                checked.value = !checked.value
+                checked = !checked
             }
         )
         Text(text = "Are you ready?")
