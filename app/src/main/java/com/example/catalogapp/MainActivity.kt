e07ee4691e24a7a6ec2ccad2e113016940017ed2
Sorongos <dosora9@gmail.com>
2025-03-25 15:24:32 +0900

    12. 프로필 카드 구현 실습

--- app/src/main/java/com/example/catalogapp/MainActivity.kt
+++ app/src/main/java/com/example/catalogapp/MainActivity.kt
@@ -4,10 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import coil3.ImageLoader
 import coil3.compose.AsyncImage
 import com.example.catalogapp.ui.theme.CatalogAppTheme
@@ -19,23 +37,63 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CatalogAppTheme {
-                Greeting()
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    CardEx(cardData)
+                    CardEx(cardData)
+                }
             }
         }
+    }
+
+    companion object {
+        val cardData = CardData(
+            imageUri = "https://picsum.photos/200/300",
+            imageDescription = "Random Image",
+            author = "Sora",
+            description = "Free Developer",
+
+            )
     }
 }
 
 @Composable
-fun Greeting() {
-    Column {
-        AsyncImage(
-            model = "https://picsum.photos/200/300",
-            contentDescription = "랜덤 이미지"
-        )
-        AsyncImage(
-            model = "https://picsum.photos/200/300",
-            contentDescription = "랜덤 이미지"
-        )
+fun CardEx(cardData: CardData) {
+    val placeHolderColor = Color(0xFF000000)
+
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(4.dp),
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(8.dp)
+        ) {
+            AsyncImage(
+                model = cardData.imageUri,
+                contentScale = ContentScale.Crop,
+                contentDescription = cardData.imageDescription,
+                modifier = Modifier.size(32.dp)
+                    .clip(CircleShape),
+                placeholder = ColorPainter(placeHolderColor),
+                error = ColorPainter(placeHolderColor),
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+
+            Column {
+                Text(
+                    text = cardData.author,
+                )
+                Spacer(modifier = Modifier.size(4.dp))
+                Text(
+                    text = cardData.description,
+                )
+            }
+        }
     }
 }
 
@@ -45,6 +103,13 @@ fun Greeting() {
 @Composable
 fun GreetingPreview() {
     CatalogAppTheme {
-        Greeting()
+        CardEx(MainActivity.cardData)
     }
 }
+
+data class CardData(
+    val imageUri: String,
+    val imageDescription: String,
+    val author: String,
+    val description: String,
+)
