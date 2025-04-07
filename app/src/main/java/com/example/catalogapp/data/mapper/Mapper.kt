910acef0c2678a14f9029fb341f4758402f392c9
Sorongos <dosora9@gmail.com>
2025-04-08 01:20:36 +0900

    fix: 앱 드로워블 이용하는 코드 제거

--- app/src/main/java/com/example/catalogapp/data/mapper/Mapper.kt
+++ app/src/main/java/com/example/catalogapp/data/mapper/Mapper.kt
@@ -5,19 +5,10 @@ import com.example.catalogapp.data.dto.CafeItemDto
 import com.example.catalogapp.domain.model.Cafe
 
 fun CafeItemDto.toDomainModel(context: Context): Cafe {
-    // 이미지 URL에서 리소스 이름 추출 (예: "/catalog/img/lion_home" -> "lion_home")
-    val imageName = imageUrl.substringAfterLast("/")
-
-    // 리소스 ID 가져오기
-    val resourceId = context.resources.getIdentifier(
-        imageName, "drawable", context.packageName
-    )
-
     return Cafe(
         imageUrl = imageUrl,
         title = title,
         description = description,
-        imageResourceId = resourceId
     )
 }
 
