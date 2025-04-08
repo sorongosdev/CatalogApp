61266084971884181e141cf7093a83bef27d46fa
Sorongos <dosora9@gmail.com>
2025-04-08 15:52:52 +0900

    feat: okhttp 연결 설정

--- app/src/main/java/com/example/catalogapp/di/RetrofitModule.kt
+++ app/src/main/java/com/example/catalogapp/di/RetrofitModule.kt
@@ -5,27 +5,47 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
-    private const val BASE_URL = "http://10.0.2.2:8080/"
+    private const val BASE_URL = "https://4a6e-222-96-111-139.ngrok-free.app/"
+
 
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit{
+    fun provideOkHttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        return OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
 
     @Provides
     @Singleton
-    fun provideApiService(retrofit: Retrofit): CatalogApiService{
-       return retrofit.create(CatalogApiService::class.java)
+    fun provideApiService(retrofit: Retrofit): CatalogApiService {
+        return retrofit.create(CatalogApiService::class.java)
     }
 }
